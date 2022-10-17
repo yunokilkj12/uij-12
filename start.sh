017125935d777ec6bac26f922ab7b03df944f523
yunokilkj12 <99712087+yunokilkj12@users.noreply.github.com>
2022-10-17 05:22:32 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-PORT=80
-UUID=5aaed9b7-7fe3-47c3-bb52-db59859ce198
+PORT=8080
+UUID=85c6d0ab-29fe-4685-b0b5-dec43ac0fc63
 WebPage=https://bing.com
 CaddyConfig=https://raw.githubusercontent.com/bsefwe/Rail/main/etc/Caddyfile
 XRayConfig=https://raw.githubusercontent.com/bsefwe/Rail/main/etc/xray.json
